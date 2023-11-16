5d073f2536bbdc2c519adcff7609c22942b883d5
Rashid <rashedhassan393.com>
2023-11-16 21:20:55 +0300

    Create Post Method useing modal

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,13 +1,21 @@
-import { Avatar, Modal } from "@mui/material";
+import { Avatar, IconButton, Modal } from "@mui/material";
 import React, { useState } from "react";
 import VideoCallIcon from "@mui/icons-material/VideoCall";
 import PhotoLibraryIcon from "@mui/icons-material/PhotoLibrary";
 import SentimentVerySatisfiedIcon from "@mui/icons-material/SentimentVerySatisfied";
+import CloseIcon from "@mui/icons-material/Close";
+import { InsertEmoticon } from "@mui/icons-material";
 import "./CSS/messagesender.css";
+import pict from "./CSS/Images/RH Logo - Copy.png";
+
 function MessageSender() {
   const [open, SetOpen] = useState(true);
   const handleClose = () => {
     SetOpen(false);
+  };
+
+  const handleOpen = () => {
+    SetOpen(true);
   };
   return (
     <>
@@ -16,6 +24,50 @@ function MessageSender() {
           <form>
             <div className="modalHeading">
               <h3>Create Post</h3>
+              <IconButton onClick={handleClose}>
+                <CloseIcon />
+              </IconButton>
+            </div>
+
+            <div className="modalHeader__top">
+              <Avatar src={pict} />
+              <h5>Rashid Hasan</h5>
+            </div>
+
+            <div className="modalBody">
+              <textarea
+                rows="5"
+                placeholder="What's on your mind Rashid ?"
+              ></textarea>
+            </div>
+
+            <div className="modalFooter">
+              <div className="modalFooterLeft">
+                <h4>Add to your post</h4>
+              </div>
+
+              <div className="modalFooterRight">
+                <IconButton>
+                  <PhotoLibraryIcon
+                    fontSize="large"
+                    style={{ color: "lightgreen" }}
+                  />
+                </IconButton>
+
+                <IconButton>
+                  <VideoCallIcon fontSize="large" style={{ color: "red" }} />
+                </IconButton>
+
+                <IconButton>
+                  <InsertEmoticon
+                    fontSize="large"
+                    style={{ color: "#ffb100" }}
+                  />
+                </IconButton>
+              </div>
+            </div>
+            <div className="input">
+              <input type="submit" className="post__submit" value="Post" />
             </div>
           </form>
         </div>
@@ -25,7 +77,11 @@ function MessageSender() {
           <Avatar />
 
           <form>
-            <input type="text" placeholder="What's on you mind Rashid?" />
+            <input
+              type="text"
+              placeholder="What's on you mind Rashid?"
+              onClick={handleOpen}
+            />
           </form>
         </div>
 
